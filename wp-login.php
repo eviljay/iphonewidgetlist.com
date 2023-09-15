bf5702b638f2f48d253da1095f5fe511f7d4e49c
evgerush <evgencherkashyn@gmail.com>
2023-09-15 11:22:15 +0300

    sd

---
+++ wp-login.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="pl-PL">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="pl-PL">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>iphonewidgetlist.com &rsaquo; Zaloguj się</title>
+	<link rel='stylesheet' id='buttons-css'  href='http://iphonewidgetlist.com/wp-includes/css/buttons.min.css?ver=4.0.1' type='text/css' media='all' />
+<link rel='stylesheet' id='open-sans-css'  href='//fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=4.0.1' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://iphonewidgetlist.com/wp-includes/css/dashicons.min.css?ver=4.0.1' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://iphonewidgetlist.com/wp-admin/css/login.min.css?ver=4.0.1' type='text/css' media='all' />
+<script type='text/javascript' src='http://iphonewidgetlist.com/wp-includes/js/jquery/jquery.js?ver=1.11.1'></script>
+<script type='text/javascript' src='http://iphonewidgetlist.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+<meta name='robots' content='noindex,follow' />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-pl-pl">
+	<div id="login">
+		<h1><a href="https://pl.wordpress.org/" title="Oparte na WordPressie" tabindex="-1">iphonewidgetlist.com</a></h1>
+	
+<form name="loginform" id="loginform" action="http://iphonewidgetlist.com/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Nazwa użytkownika<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Hasło<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Zapamiętaj mnie</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Zaloguj się" />
+		<input type="hidden" name="redirect_to" value="http://iphonewidgetlist.com/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="http://iphonewidgetlist.com/wp-login.php?action=lostpassword" title="Hasło utracone i znalezione">Nie pamiętasz hasła?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="http://iphonewidgetlist.com/" title="Potrzebujesz pomocy?">&larr; Powrót do witryny &#8222;iphonewidgetlist.com&#8221;</a></p>
+	
+	</div>
+
+	
+	<script type='text/javascript' src='http://iphonewidgetlist.com/wp-content/themes/Webly/epanel/page_templates/js/fancybox/jquery.easing-1.3.pack.js?ver=1.3.4'></script>
+<script type='text/javascript' src='http://iphonewidgetlist.com/wp-content/themes/Webly/epanel/page_templates/js/fancybox/jquery.fancybox-1.3.4.pack.js?ver=1.3.4'></script>
+<script type='text/javascript' src='http://iphonewidgetlist.com/wp-content/themes/Webly/epanel/page_templates/js/et-ptemplates-frontend.js?ver=1.1'></script>
+	<div class="clear"></div>
+	</body>
+	</html>
+	
